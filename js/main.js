01243beb5aae109dcd503cf573e3c6b2913f39de
Blackhawk <yaroslavvlasenko@gmail.com>
2016-09-06 12:15:27 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+  $('.slider').slick({
+    dots: false
+  });
+});
+
+$(function(){
+  $('#menu').slicknav({
+    prependTo: '.menu-wrap' /* move to menu-wrap */
+  });
+});
